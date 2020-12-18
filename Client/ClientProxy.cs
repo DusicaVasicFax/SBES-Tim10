283a9c5ac05cc541020f1f846606a00b5c1a5d6e
Kristian Tot <kristiantot20@gmail.com>
2020-12-18 18:57:45 +0100

    inital commit

---
+++ Client/ClientProxy.cs
@@ -0,0 +1,33 @@
+﻿using ServiceContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class ClientProxy : ChannelFactory<ISecurityService>, ISecurityService, IDisposable
+    {
+        ISecurityService factory;
+
+        public ClientProxy(NetTcpBinding binding, string address) : base(binding, address)
+        {
+            factory = this.CreateChannel();
+        }
+
+        public void AddUser(string username, string password)
+        {
+
+            try
+            {
+                factory.AddUser(username, password);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+        }
+    }
+}

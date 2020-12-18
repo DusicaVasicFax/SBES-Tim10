d604a592cd7f8309543dedf3f2a9b4277a077ddd
Dusan Mazic <dusanmazic123@gmail.com>
2020-12-18 18:52:44 +0100

    Initial push

---
+++ ServiceContracts/ISecurityService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts
+{
+	[ServiceContract]
+	public interface ISecurityService
+	{
+		[OperationContract]
+		void AddUser(string username, string password);
+
+	}
+}

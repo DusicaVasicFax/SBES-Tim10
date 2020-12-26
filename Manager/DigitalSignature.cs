04785c13a9dbc6679a36a86268aa5e447b5b1583
Kristian Tot <kristiantot20@gmail.com>
2020-12-26 16:21:44 +0100

    Made the digital signature manager

---
+++ Manager/DigitalSignature.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    public enum HashAlgorithms { SHA1, SHA256 }
+
+    public class DigitalSignature
+    {
+        public static byte[] Create(string message, HashAlgorithm hashAlgorithm, X509Certificate2 certificate)
+        {
+            RSACryptoServiceProvider csp = (RSACryptoServiceProvider)certificate.PrivateKey;
+
+            if (csp == null) throw new Exception("Valid certificate was not found");
+
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            byte[] data = encoding.GetBytes(message); //current message encripted in bytes
+            byte[] hash = null; //hash result
+
+            if (hashAlgorithm.Equals(HashAlgorithms.SHA1))
+            {
+                SHA1Managed sha1 = new SHA1Managed();
+                hash = sha1.ComputeHash(data);
+            }
+            else if (hashAlgorithm.Equals(HashAlgorithms.SHA256))
+            {
+                SHA256Managed sha256 = new SHA256Managed();
+                hash = sha256.ComputeHash(data);
+            }
+
+            return csp.SignHash(hash, CryptoConfig.MapNameToOID(hashAlgorithm.ToString()));
+        }
+
+        public static bool Verify(string message, HashAlgorithm hashAlgorithm, byte[] signature, X509Certificate2 certificate)
+        {
+            RSACryptoServiceProvider csp = (RSACryptoServiceProvider)certificate.PublicKey.Key;
+            UnicodeEncoding encoding = new UnicodeEncoding();
+
+            byte[] data = encoding.GetBytes(message);
+            byte[] hash = null;
+
+            if (hashAlgorithm.Equals(HashAlgorithms.SHA1))
+            {
+                SHA1Managed sha1 = new SHA1Managed();
+                hash = sha1.ComputeHash(data);
+            }
+            else if (hashAlgorithm.Equals(HashAlgorithms.SHA256))
+            {
+                SHA256Managed sha256 = new SHA256Managed();
+                hash = sha256.ComputeHash(data);
+            }
+
+            return csp.VerifyHash(hash, CryptoConfig.MapNameToOID(hashAlgorithm.ToString()), signature);
+        }
+    }
+}

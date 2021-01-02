65750a7e0f011bcb39426b7277352ea388c6f460
sdnikola <sdnikola72@gmail.com>
2021-01-02 13:22:06 +0100

    menu

--- Client/Program.cs
+++ Client/Program.cs
@@ -29,13 +29,49 @@ namespace Client
                 X509Certificate2 clientCERT = CertManager.GetCertificateFromStorage(StoreName.My,
                 StoreLocation.LocalMachine, signCertCN);
 
-                byte[] signature = DigitalSignature.Create("Ovde treba fajl ili deo fajla da se salje", HashAlgorithms.SHA1, clientCERT);
+
                 //proxy.AddFile("file5.txt", signature, "First file");
                 ////  proxy.EditFile("file1.txt", signature,"First file again");
                 //proxy.DeleteFile("file5.txt");
+                byte[] signature = DigitalSignature.Create("Ovde treba fajl ili deo fajla da se salje", HashAlgorithms.SHA1, clientCERT);
+                Console.WriteLine("Connected to Services");
+                
+                while (true)
+                {
+                    Console.WriteLine("*****************Menu*****************");
+                    Console.WriteLine("1. Doda fajl\n2. Obrisi fajln3. Azuriraj fajl");
+                    Console.Write("Choose option: ");
+                    int choice = int.Parse(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Unesite ime fajla u kom zelite da usete tekst");
+                            string filename = Console.ReadLine();
+                            Console.WriteLine("Unesite tekst koji zelite da upisete u fajl");
+                            string text = Console.ReadLine();
+                            proxy.AddFile(filename, signature, text);
+                            break;
+                        case 2:
+                            Console.WriteLine("Unesite ime fajla koje zelite da obrisite");
+                            filename = Console.ReadLine();
+                            proxy.DeleteFile(filename);
+                            break;
+                        case 3:
+                            Console.WriteLine("Unesite ime fajla u kom zelite da promenite tekst");
+                            filename = Console.ReadLine();
+                            Console.WriteLine("Unesite tekst koji zelite da izmenite");
+                            text = Console.ReadLine();
+                            proxy.AddFile(filename, signature, text);
+                            break;
+                        default:
+                            Console.WriteLine("Niste uneli ispravnu opciju.");
+                            break;
+                    }
+                }
+
             }
 
-            Console.ReadLine();
         }
+
     }
 }

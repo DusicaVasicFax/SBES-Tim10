95ff8d7a90fd6db2e6fc717635ae017d8bf10977
Kristian Tot <kristiantot20@gmail.com>
2021-01-03 14:00:34 +0100

    FileManager proxy left to finish in ips service

--- IPSService/IPSService.cs
+++ IPSService/IPSService.cs
@@ -9,7 +9,22 @@ namespace IPSService
         {
             Audit.CriticalLog(alarm);
             Console.WriteLine("Critical");
-            //TODO delete the file
+
+            //NetTcpBinding binding = new NetTcpBinding();
+            //string address = "net.tcp://localhost:9999/SecurityService";
+
+            //binding.Security.Mode = SecurityMode.Transport;
+            //binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+            //binding.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
+
+            //Console.WriteLine("Korisnik koji je pokrenuo klijenta je : " + WindowsIdentity.GetCurrent().Name);
+
+            //EndpointAddress endpointAddress = new EndpointAddress(new Uri(address),
+            //    EndpointIdentity.CreateUpnIdentity("wcfServer"));
+
+            //using (FileManagerProxy proxy = new FileManagerProxy(binding, endpointAddress))
+            //{
+            //}
         }
 
         public void InformationLog(Alarm alarm)
--- IPSService/Program.cs
+++ IPSService/Program.cs
@@ -12,22 +12,6 @@ namespace IPSService
     {
         private static void Main(string[] args)
         {
-            //NetTcpBinding binding = new NetTcpBinding();
-            //string address = "net.tcp://localhost:9999/SecurityService";
-
-            //binding.Security.Mode = SecurityMode.Transport;
-            //binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
-            //binding.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
-
-            //Console.WriteLine("Korisnik koji je pokrenuo klijenta je : " + WindowsIdentity.GetCurrent().Name);
-
-            //EndpointAddress endpointAddress = new EndpointAddress(new Uri(address),
-            //    EndpointIdentity.CreateUpnIdentity("wcfServer"));
-
-            //using (FileManagerProxy proxy = new FileManagerProxy(binding, endpointAddress))
-            //{
-            //}
-
             Console.ReadLine();
             string ipsCert = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
             //Console.log(ipsCert);

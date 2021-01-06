4e05568d78d125320f621be554483819f7c5a0af
Kristian Tot <kristiantot20@gmail.com>
2021-01-06 21:06:58 +0100

    IPS service finished

--- IPSService/Program.cs
+++ IPSService/Program.cs
@@ -14,40 +14,37 @@ namespace IPSService
         {
             Console.ReadLine();
             string ipsCert = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);//autetntifikacija putem sertifikata
-            //Console.log(ipsCert);
-
             NetTcpBinding binding = new NetTcpBinding();
             binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;//autetntifikacija putem sertifikata
 
-            string address = "net.tcp://localhost:8888/IIPSService";            
+            string address = "net.tcp://localhost:8888/IIPSService";
 
             ServiceHost host = new ServiceHost(typeof(IPSService));
             host.AddServiceEndpoint(typeof(IIPSService), binding, address);
-            ///Custom validation mode enables creation of a custom validator - CustomCertificateValidator
+
             host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.Custom;//autetntifikacija putem sertifikata
             host.Credentials.ClientCertificate.Authentication.CustomCertificateValidator = new ServiceCertValidator();//autetntifikacija putem sertifikata
 
-            ///If CA doesn't have a CRL associated, WCF blocks every client because it cannot be validated
             host.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
 
-            ///Set appropriate service's certificate on the host. Use CertManager class to obtain the certificate based on the "srvCertCN"
             host.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, ipsCert);//autetntifikacija putem sertifikata
-
-
+            IPSService iPSService = null;
             try
             {
                 host.Open();
                 Console.WriteLine("IPS service is started.\nPress <enter> to stop ...");
-                IPSService iPSService = new IPSService();
+                iPSService = new IPSService();
                 Console.ReadLine();
             }
             catch (Exception e)
             {
+                Console.WriteLine("Something went wrong while starting IPS service");
                 Console.WriteLine("[ERROR] {0}", e.Message);
                 Console.WriteLine("[StackTrace] {0}", e.StackTrace);
             }
             finally
-            {               
+            {
+                iPSService.Dispose();
                 host.Close();
             }
         }
